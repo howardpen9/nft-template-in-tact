5e645bbba5b077ddcc5fc6a1f785337f574b2b9c
Steve Korshakov <steve@korshakov.com>
2023-01-03 22:28:45 +0400

    fix: fix deploy code

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,5 +1,5 @@
-import { contractAddress, toNano } from "ton";
-import { packAdd, SampleTactContract_init } from "./output/sample_SampleTactContract";
+import { beginCell, contractAddress, toNano } from "ton";
+import { SampleTactContract, storeAdd } from "./output/sample_SampleTactContract";
 import { printAddress, printDeploy, printHeader } from "./utils/print";
 import { randomAddress } from "./utils/randomAddress";
 
@@ -7,9 +7,9 @@ import { randomAddress } from "./utils/randomAddress";
 
     // Parameters
     let owner = randomAddress(0, 'some-owner'); // Replace owner with your address
-    let packed = packAdd({ $$type: 'Add', amount: 10n }); // Replace if you want another message used
-    let init = await SampleTactContract_init(owner);
-    let address = contractAddress({ workchain: 0, initialCode: init.code, initialData: init.data });
+    let packed = beginCell().store(storeAdd({ $$type: 'Add', amount: 10n })).endCell(); // Replace if you want another message used
+    let init = await SampleTactContract.init(owner);
+    let address = contractAddress(0, init);
     let deployAmount = toNano(10);
     let testnet = true;
 
--- sources/utils/print.ts
+++ sources/utils/print.ts
@@ -1,5 +1,4 @@
-import { Address, Cell, contractAddress, StateInit } from "ton";
-import BN from 'bn.js';
+import { Address, beginCell, Cell, contractAddress, storeStateInit } from "ton";
 import qs from 'qs';
 
 export function printSeparator() {
@@ -13,32 +12,34 @@ export function printHeader(name: string) {
 }
 
 export function printAddress(address: Address, testnet: boolean = true) {
-    console.log("Address: " + address.toFriendly({ testOnly: testnet }));
-    console.log("Explorer: " + "https://" + (testnet ? 'testnet.' : '') + "tonwhales.com/explorer/address/" + address.toFriendly({ testOnly: testnet }));
+    console.log("Address: " + address.toString({ testOnly: testnet }));
+    console.log("Explorer: " + "https://" + (testnet ? 'testnet.' : '') + "tonwhales.com/explorer/address/" + address.toString({ testOnly: testnet }));
     printSeparator();
 }
 
-export function printDeploy(init: { code: Cell, data: Cell }, amount: BN, command: Cell | string, testnet: boolean = true) {
+export function printDeploy(init: { code: Cell, data: Cell }, value: bigint, command: Cell | string, testnet: boolean = true) {
 
     // Resolve target address
-    let to = contractAddress({ workchain: 0, initialCode: init.code, initialData: init.data });
+    let to = contractAddress(0, init);
 
     // Resovle init
-    let cell = new Cell();
-    new StateInit(init).writeTo(cell);
-    let initStr = cell.toBoc({ idx: false }).toString("base64");
+    let initStr = beginCell()
+        .store(storeStateInit(init))
+        .endCell()
+        .toBoc()
+        .toString('base64');
 
     let link: string;
     if (typeof command === 'string') {
-        link = `https://${testnet ? 'test.' : ''}tonhub.com/transfer/` + to.toFriendly({ testOnly: testnet }) + "?" + qs.stringify({
+        link = `https://${testnet ? 'test.' : ''}tonhub.com/transfer/` + to.toString({ testOnly: testnet }) + "?" + qs.stringify({
             text: command,
-            amount: amount.toString(10),
+            amount: value.toString(10),
             init: initStr
         });
     } else {
-        link = `https://${testnet ? 'test.' : ''}tonhub.com/transfer/` + to.toFriendly({ testOnly: testnet }) + "?" + qs.stringify({
+        link = `https://${testnet ? 'test.' : ''}tonhub.com/transfer/` + to.toString({ testOnly: testnet }) + "?" + qs.stringify({
             text: "Deploy contract",
-            amount: amount.toString(10),
+            amount: value.toString(10),
             init: initStr,
             bin: command.toBoc({ idx: false }).toString('base64'),
         });
