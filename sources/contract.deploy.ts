d361b2354b3594d9857b8ba78b9f4031b0331d4d
Steve Korshakov <steve@korshakov.com>
2023-01-03 22:57:51 +0400

    ref: replace randomAddress with testAddress from ton-emulator

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,12 +1,12 @@
 import { beginCell, contractAddress, toNano } from "ton";
+import { testAddress } from "ton-emulator";
 import { SampleTactContract, storeAdd } from "./output/sample_SampleTactContract";
 import { printAddress, printDeploy, printHeader } from "./utils/print";
-import { randomAddress } from "./utils/randomAddress";
 
 (async () => {
 
     // Parameters
-    let owner = randomAddress(0, 'some-owner'); // Replace owner with your address
+    let owner = testAddress('some-owner'); // Replace owner with your address
     let packed = beginCell().store(storeAdd({ $$type: 'Add', amount: 10n })).endCell(); // Replace if you want another message used
     let init = await SampleTactContract.init(owner);
     let address = contractAddress(0, init);
