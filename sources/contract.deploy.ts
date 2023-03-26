62ae2f1c30e12fdbb371dde5cd5f421611886a5e
Howard Peng <new_jason_howard@proton.me>
2023-03-26 23:56:09 +0800

    Updates

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -13,13 +13,17 @@ import { NftCollection } from "./output/sample_NftCollection";
     let body = beginCell().storeUint(0, 32).storeStringTail("Mint").endCell();
 
     // Parameters
-    let owner = Address.parse("YOUR_ADDRESS"); // Replace owner with your address
-    let init = await NftCollection.init(owner, newContent, {
-        $$type: "RoyaltyParams",
-        numerator: 350n, // 350n = 35%
-        denominator: 1000n,
-        destination: owner,
-    });
+    let owner = Address.parse(""); // Replace owner with your address
+    let init = await NftCollection.init(
+        owner, 
+        newContent, 
+        {
+            $$type: "RoyaltyParams",
+            numerator: 350n, // 350n = 35%
+            denominator: 1000n,
+            destination: owner,
+        }
+    );
 
     let address = contractAddress(0, init);
     let deployAmount = toNano("0.5");
